2dcc3ffa47ecf7a93727d9454bbbc508a731e26d
Ciber-marcelo <marcelones125@gmail.com>
2024-01-08 21:41:23 -0300

    feat: create header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { FaChevronDown } from "react-icons/fa6";
+import { CgMenuLeftAlt } from "react-icons/cg";
+
+export default function Header() {
+   return (
+      <div className="flex justify-between fixed w-full px-8 bg-color1 h-[72px]">
+         <div className="flex items-center gap-4">
+            <CgMenuLeftAlt size='24' color='white' />
+            <Image
+               width={126}
+               height={20}
+               src='/images/SUA LOGO.svg'
+               alt='logo'
+            />
+         </div>
+
+         <div className="flex items-center gap-4">
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-color2 text-white font-[600]">N</div>
+            <span className="text-xs text-white font-[600]">Nome do Usuário Aqui</span>
+            <FaChevronDown size='24' color='white' />
+         </div>
+      </div>
+   )
+}
