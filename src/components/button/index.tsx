639e3b6185d3e49b56c67ef7b9ae6fe4771eaad8
Ciber-marcelo <marcelones125@gmail.com>
2024-01-09 19:01:38 -0300

    feat: create button

---
+++ src/components/button/index.tsx
@@ -0,0 +1,29 @@
+type buttonProps = {
+   name: string
+   onClick?: any
+}
+
+export default function Button({ name, onClick }: buttonProps) {
+   return (
+      <div className="max-w-[584px]">
+         <button
+            onClick={onClick}
+            className="
+            bg-color6 
+            border-color5 
+            rounded-full 
+            h-[56px]
+            px-16 
+            hover:bg-color3
+            transition-all
+            text-[18px]
+            font-[700]
+            text-color1 
+            capitalize
+            "
+         >
+            {name}
+         </button>
+      </div>
+   )
+}
